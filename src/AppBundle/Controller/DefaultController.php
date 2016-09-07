a7663ec938723ca3fe0cf543b972141a64ab05bc
manueldelapenna <manueldelapenna@gmail.com>
2016-09-07 15:13:06 -0300

    ajustes

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,7 +11,7 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
-     * @Security("has_role('ROLE_ADMIN')")
+     * @Security("has_role('ROLE_USER')")
      */
     public function indexAction(Request $request)
     {
